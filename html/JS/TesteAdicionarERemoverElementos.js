2dec07db6e9adb4c1c5a99d283ab3c38f2a43b1e
Marvin Mendes <marvin.m.lopes@gmail.com>
2021-06-11 01:04:11 -0300

    Conhecendo métodos POP e SHIFT em arrays

---
+++ html/JS/TesteAdicionarERemoverElementos.js
@@ -0,0 +1,18 @@
+var numbers = []
+
+numbers.push(1)
+numbers.push(2)
+numbers.push(3)
+numbers.push(5)
+numbers.push(7)
+numbers.push(11)
+
+console.log(numbers)
+
+numbers.pop()
+console.log("depois do POP " + numbers + " removeu o último elemento")
+
+numbers.shift()
+console.log("depois do SHIFT " + numbers + " removeu o primeiro elemento")
+
+
--- html/JS/TesteArrays.js
+++ html/JS/TesteArrays.js
@@ -6,5 +6,5 @@ fibonacci[2] = 1
 
 for(let i = 3; i < 20; i++) {
     fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
-    console.log(fibonacci[i])
+    //console.log(fibonacci[i])
 }
