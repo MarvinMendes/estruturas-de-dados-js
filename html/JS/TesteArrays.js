6f6f53e5bd9f73c8dc4add16d93d71b8b3c854cb
Marvin Mendes <marvin.m.lopes@gmail.com>
2021-06-11 00:54:05 -0300

    first commit

---
+++ html/JS/TesteArrays.js
@@ -0,0 +1,10 @@
+var fibonacci = []
+
+fibonacci[0] = 0
+fibonacci[1] = 1
+fibonacci[2] = 1
+
+for(let i = 3; i < 20; i++) {
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
+    console.log(fibonacci[i])
+}
