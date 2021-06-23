fefed04000b85b9b53358b734a97d88ae786e3e7
Marvin Mendes <marvin.m.lopes@gmail.com>
2021-06-23 03:20:16 -0300

    Praticando o conceito de Fila com um jogo O jogo é o tradicional jogo da batata quente

--- src/JS/fila/ImplementacaoFila.js
+++ src/JS/fila/ImplementacaoFila.js
@@ -32,3 +32,41 @@ console.log('Tamanho da fila = ' + fila.size())
 
 console.log('Primeiro da fila ' + fila.front())
 
+
+/*
+    abaixo uma implementação de um jogo da batata quente
+
+    o primeiro parâmetro é um array com os nomes dos jogadores
+    o segundo o numero de jogadas
+    eles trocarão de lugar 
+    a cada jogada o primeiro da fila irá para o final da fila
+    quem permanecer no início da fila ao final da rodada será eliminado
+*/
+
+const hotPotato = function(namesArray, rounds) {
+
+    var queue = new Queue()
+
+    for(let i = 0; i < namesArray.length; i++) {
+        queue.enqueue(namesArray[i])
+    }
+
+    var eliminated = ''
+
+    while (queue.size() > 1) {
+        for(let i =0; i < rounds; i++) {
+            queue.enqueue(queue.dequeue())
+        }
+
+        eliminated = queue.dequeue()
+        console.log(eliminated + ' foi eliminado do jogo')
+    }
+
+    return queue.dequeue()
+
+}
+var nomes = ['Marvin', 'Ana', 'Kelvin', 'Marcelo', 'Tay']
+
+var game = hotPotato(nomes, 7)
+
+console.log('O jogo foi ganho por ' + game)
